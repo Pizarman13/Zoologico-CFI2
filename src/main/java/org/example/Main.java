17bf3644c54d33e9f2d545a1b3d3007272590231
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2024-03-13 20:44:43 +0100

    tour

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,13 +8,13 @@ import org.example.CuidadoAnimales.Animal;
 
 public class Main {
 
-    AnAcuatico pinguino = new AnAcuatico("Agua salada", "Pinguino", "Ave", "Pez", 2, 100);
-    AnAviario aguila = new AnAviario(true, "Aguila", "Ave", "Conejo", 3, 100);
-    AnTerrestre leon = new AnTerrestre(4, "Leon", "Mamifero", "Carnivoro", 5, 100);
-    AnTerrestre avestruz = new AnTerrestre(2, "Avestruz", "Ave", "Herbivoro", 4, 100);
-    AnAcuatico tortuga = new AnAcuatico("Agua salada", "Tortuga", "Reptil", "Herbivoro", 1, 100);
-    AnAviario loro = new AnAviario(true, "Loro", "Ave", "Omnivoro", 2, 100);
-    AnAcuatico castor = new AnAcuatico("Agua dulce", "Castor", "Mamifero", "Omnivoro", 3, 100);
+    static AnAcuatico pinguino = new AnAcuatico("Agua salada", "Pinguino", "Ave", "Pez", 2, 100);
+    static AnAviario aguila = new AnAviario(true, "Aguila", "Ave", "Conejo", 3, 100);
+    static AnTerrestre leon = new AnTerrestre(4, "Leon", "Mamifero", "Carnivoro", 5, 100);
+    static AnTerrestre avestruz = new AnTerrestre(2, "Avestruz", "Ave", "Herbivoro", 4, 100);
+    static AnAcuatico tortuga = new AnAcuatico("Agua salada", "Tortuga", "Reptil", "Herbivoro", 1, 100);
+    static AnAviario loro = new AnAviario(true, "Loro", "Ave", "Omnivoro", 2, 100);
+    static AnAcuatico castor = new AnAcuatico("Agua dulce", "Castor", "Mamifero", "Omnivoro", 3, 100);
 
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
@@ -45,6 +45,77 @@ public class Main {
 
 
 
+        }
+    }
+
+    public static void Tour() {
+        System.out.println("Bienvenido al tour del Zoologico");
+        System.out.println("1. Acuaticos");
+        System.out.println("2. Terrestres");
+        System.out.println("3. Aviarios");
+        System.out.println("4. Salir");
+        System.out.println("Escoge una opcion: ");
+        try {
+            int opcion = sc.nextInt();
+            if (opcion == 1) {
+                System.out.println("Bienvenido a la seccion de animales acuaticos");
+                System.out.println("1. Pinguino");
+                System.out.println("2. Tortuga");
+                System.out.println("3. Castor");
+                System.out.println("4. Regresar");
+                System.out.println("Escoge una opcion: ");
+                int opcion2 = sc.nextInt();
+                if (opcion2 == 1) {
+                    System.out.println(pinguino.toString());
+                } else if (opcion2 == 2) {
+                    System.out.println(tortuga.toString());
+                } else if (opcion2 == 3) {
+                    System.out.println(castor.toString());
+                } else if (opcion2 == 4) {
+                    Tour();
+                } else {
+                    System.out.println("Opcion no valida");
+                }
+            } else if (opcion == 2) {
+                System.out.println("Bienvenido a la seccion de animales terrestres");
+                System.out.println("1. Leon");
+                System.out.println("2. Avestruz");
+                System.out.println("3. Regresar");
+                System.out.println("Escoge una opcion: ");
+                int opcion2 = sc.nextInt();
+                if (opcion2 == 1) {
+                    System.out.println(leon.toString());
+                } else if (opcion2 == 2) {
+                    System.out.println(avestruz.toString());
+                } else if (opcion2 == 3) {
+                    Tour();
+                } else {
+                    System.out.println("Opcion no valida");
+                }
+            } else if (opcion == 3) {
+                System.out.println("Bienvenido a la seccion de animales aviarios");
+                System.out.println("1. Aguila");
+                System.out.println("2. Loro");
+                System.out.println("3. Regresar");
+                System.out.println("Escoge una opcion: ");
+                int opcion2 = sc.nextInt();
+                if (opcion2 == 1) {
+                    System.out.println(aguila.toString());
+                } else if (opcion2 == 2) {
+                    System.out.println(loro.toString());
+                } else if (opcion2 == 3) {
+                    Tour();
+                } else {
+                    System.out.println("Opcion no valida");
+                }
+            } else if (opcion == 4) {
+                System.out.println("Gracias por visitar el Zoologico");
+            } else {
+                System.out.println("Opcion no valida");
+            }
+        } catch (Exception e) {
+            System.out.println("Opcion no valida");
+            sc.next();
         }
     }
 }
